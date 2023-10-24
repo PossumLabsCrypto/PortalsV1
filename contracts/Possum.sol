9de46ad685ab2f967ef7df48da9bd15578f54a4e
PossumLabsCrypto <136654177+PossumLabsCrypto@users.noreply.github.com>
2023-10-24 23:52:48 +0200

    Update Possum.sol

--- contracts/Possum.sol
+++ contracts/Possum.sol
@@ -2,10 +2,13 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 contract Possum is ERC20, ERC20Permit {
-    constructor(uint256 _totalSupply) ERC20("Possum", "PSM") ERC20Permit("Possum"){
+    constructor(
+        uint256 _totalSupply
+    ) ERC20("Possum", "PSM") ERC20Permit("Possum") {
         _mint(msg.sender, _totalSupply); // mint initial supply to deployer
     }
-}   
+}
