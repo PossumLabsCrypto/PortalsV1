8b96fbf6330e7712c2f9b4d56031d52a59a43a27
PossumLabsCrypto <136654177+PossumLabsCrypto@users.noreply.github.com>
2023-10-24 14:58:29 +0200

    Update MintBurnToken.sol
    added permit by signature

--- contracts/MintBurnToken.sol
+++ contracts/MintBurnToken.sol
@@ -3,10 +3,11 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MintBurnToken is ERC20, ERC20Burnable, Ownable(msg.sender) {
-    constructor() ERC20("Placeholder", "ABC") {}
+contract MintBurnToken is ERC20, ERC20Burnable, Ownable, ERC20Permit {
+    constructor() ERC20("PrincipalToken", "PRINCIPAL") ERC20Permit("PrincipalToken"){}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
