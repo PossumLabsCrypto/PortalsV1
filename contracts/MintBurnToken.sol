6acfae5ca31259d91e3d936239e440bd849948d5
PossumLabsCrypto <136654177+PossumLabsCrypto@users.noreply.github.com>
2023-11-27 01:09:45 +0100

    Update MintBurnToken.sol

--- contracts/MintBurnToken.sol
+++ contracts/MintBurnToken.sol
@@ -1,15 +1,19 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+// SPDX-License-Identifier: GPL-2.0-only
+pragma solidity =0.8.19;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract MintBurnToken is ERC20, ERC20Burnable, Ownable(msg.sender), ERC20Permit {
-    constructor() ERC20("PrincipalToken", "PRINCIPAL") ERC20Permit("PrincipalToken"){}
+contract MintBurnToken is ERC20, ERC20Burnable, Ownable, ERC20Permit {
+    constructor(
+        address initialOwner,
+        string memory name,
+        string memory symbol
+    ) ERC20(name, symbol) Ownable(initialOwner) ERC20Permit(name) {}
 
-    function mint(address to, uint256 amount) public onlyOwner {
+    function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
     }
 }
