3ae1fdf372a1a635f3f3db39d7af088c2230040e
defichad23 <alex@possumfinance.com>
2023-11-27 06:09:20 +0100

    update to Foundry

--- contracts/interfaces/IStaking.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
-
-interface IStaking {
-    function deposit(address _to, uint256 _amount) external;
-
-    function withdraw(uint256 _amount) external;
-}
---
+++ src/interfaces/IStaking.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.19;
+
+interface IStaking {
+    function deposit(address _to, uint256 _amount) external;
+
+    function withdraw(uint256 _amount) external;
+}
